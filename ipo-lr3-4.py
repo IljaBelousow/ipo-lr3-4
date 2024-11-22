a820976438d449e2da89ce83f0bd87a93b52b118
ПИКМИ МЕН <103372208+IljaBelousow@users.noreply.github.com>
2024-11-22 15:15:47 +0300

    Add files via upload

---
+++ ipo-lr3-4.py
@@ -0,0 +1,9 @@
+a = int(input("vvedite prevoe chislo "))
+b = int(input("vvedite vtoroe chislo "))
+c = int(input("vvedite tret'e chislo "))
+if a > b and a > c:
+    print("pervoe chislo boslhe vseh")
+elif b > c and b > c:
+    print("vtoroe chislo boslhe vseh")
+elif c > b and c > a:
+    print("tret'e chislo boslhe vseh")
